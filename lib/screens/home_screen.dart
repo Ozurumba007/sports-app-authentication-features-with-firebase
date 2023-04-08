bb9c99944f5628dcbb3bd05ba0fd7704acd5ed26
Ozurumba Onwuasoanya <onwuasoanyaozurumba007@gmail.com>
2023-04-08 19:44:42 +0100

    built the user interface of the login screen \ created files (home_screen, login_screen, forgot_password, register_page) \ created a folder called screen to add all the newly created files in it \ created a project in firebase \ implemented the steps in adding firebase to your local project \ added firebase dependencies in the pubspec.yaml file \ added the google service Json file to the android/app path \ initialized firebase email and password login and logout \ commiting all code to the repository

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:sports_app/screens/login_screen.dart';
+import 'package:get/get.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final user = FirebaseAuth.instance.currentUser!;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text('Signed In as ' + user.email!),
+          MaterialButton(
+            onPressed: () {
+              FirebaseAuth.instance.signOut();
+            },
+            color: Colors.deepPurple[200],
+            child: Text('Sign Out '),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/screens/register_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class RegisterScreen extends StatefulWidget {
+  RegisterScreen({Key? key}) : super(key: key);
+
+  @override
+  State<RegisterScreen> createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [],
+      ),
+    );
+  }
+}
