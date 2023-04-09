5860afd6a09038aed771f05d6b956b39ab384e11
Ozurumba Onwuasoanya <onwuasoanyaozurumba007@gmail.com>
2023-04-09 12:23:50 +0100

    added a drop_menu file for the special interest to be picked by the user

---
+++ lib/widgets/drop_menu.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DropDownMenu extends StatefulWidget {
+  DropDownMenu({Key? key}) : super(key: key);
+
+  @override
+  State<DropDownMenu> createState() => _DropDownMenuState();
+}
+
+class _DropDownMenuState extends State<DropDownMenu> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
