69aef7891e855fb571ba19134a3d727115693302
Ozurumba Onwuasoanya <onwuasoanyaozurumba007@gmail.com>
2023-04-12 15:14:50 +0100

    added a new file called get user name

--- lib/bottom_navigation_pages/home_page.dart
+++ lib/bottom_navigation_pages/home_page.dart
@@ -1,12 +1,57 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class HomeNavBar extends StatelessWidget {
+class HomeNavBar extends StatefulWidget {
   const HomeNavBar({Key? key}) : super(key: key);
+
+  @override
+  State<HomeNavBar> createState() => _HomeNavBarState();
+}
+
+class _HomeNavBarState extends State<HomeNavBar> {
+  final user = FirebaseAuth.instance.currentUser!;
+
+  List<String> docIDs = [];
+
+  Future getDocId() async {
+    await FirebaseFirestore.instance.collection('users').get().then(
+          (snapshot) => snapshot.docs.forEach(
+            (document) {
+              print(document.reference);
+              docIDs.add(document.reference.id);
+            },
+          ),
+        );
+  }
+
+  @override
+  void initState() {
+    getDocId();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('Home')),
+      body: Column(
+        children: [
+          Center(
+            child: Text('Welcome' + user.email!),
+          ),
+          Expanded(
+              child: FutureBuilder(
+            future: getDocId(),
+            builder: (context, snapshot) {
+              return ListView.builder(itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(docIDs[index]),
+                );
+              });
+            },
+          )),
+        ],
+      ),
     );
   }
 }
